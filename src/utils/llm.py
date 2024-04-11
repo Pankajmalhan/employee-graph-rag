7b52c9a60b8a338639fbf3e5a2870b20f30cba1b
pankajmalhan <pankajmalhan30@gmail.com>
2024-04-11 10:56:21 +0530

    added extract_entities_relationships method

--- src/utils/llm.py
+++ src/utils/llm.py
@@ -1,11 +1,14 @@
 import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
 from langchain_core.messages import SystemMessage, HumanMessage
 from langchain.prompts import HumanMessagePromptTemplate
 from langchain.globals import set_llm_cache
 from langchain.cache import SQLiteCache
+from timeit import default_timer as timer
+import glob
+import json
 
 set_llm_cache(SQLiteCache(database_path=".langchain.db"))
 load_dotenv()
@@ -30,4 +33,26 @@ def process_gpt(file_prompt, system_msg):
         ])
    chain = chat_template | llm
    nlp_results =  chain.invoke({})
-   return nlp_results
+   return nlp_results.content
+
+
+# Function to take folder of files and a prompt template, and return a json-object of all the entities and relationships
+def extract_entities_relationships(folder, prompt_template):
+    start = timer()
+    files = glob.glob(f"./data/{folder}/*")
+    system_msg = "You are a helpful IT-project and account management expert who extracts information from documents."
+    print(f"Running pipeline for {len(files)} files in {folder} folder")
+    results = []
+    for i, file in enumerate(files):
+        print(f"Extracting entities and relationships for {file}")
+        try:
+            with open(file, "r") as f:
+                text = f.read().rstrip()
+                prompt = PromptTemplate.from_template(prompt_template).invoke({"ctext":text}).to_string()
+                result = process_gpt(prompt, system_msg=system_msg)
+                results.append(json.loads(result))
+        except Exception as e:
+            print(f"Error processing {file}: {e}")
+    end = timer()
+    print(f"Pipeline completed in {end-start} seconds")
+    return results
